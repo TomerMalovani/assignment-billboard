04a64e29a8f656e55956b89eee8ac04ac971685d
Tomer Malovani <tomermalovani@gmail.com>
2019-08-06 15:25:52 +0300

    basic page was tested and now working

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'billboard',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/billboard/urls.py
+++ backend/billboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.contrib.auth.views import login, logout
 
 urlpatterns = [
-    url(r'^$', views.get_activities, name='get_activites'),
+    url(r'^$', views.index, name='index'),
     url(r'^login', login, name='login'),
     url(r'^logout', logout, name='logout')
 ]
